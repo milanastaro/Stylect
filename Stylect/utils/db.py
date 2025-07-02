4ee26e97a5977c1a8da4aeef4efe68ecc29bb5fe
milanastaro <milananemkov@gmail.com>
2025-07-02 09:16:16 -0700

    Delete Stylect directory

--- Stylect/utils/db.py
+++
@@ -1,7 +0,0 @@
-def get_user_color_palette(username):
-    path = f"data/users/{username}_palette.json"
-    if os.path.exists(path):
-        import json
-        with open(path, "r") as f:
-            return json.load(f).get("colors", [])
-    return []
